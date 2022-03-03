5b3e192cd41186e28347813afb3fa267247acd5e
OyugoObonyo <nyikal1999@gmail.com>
2022-03-03 20:39:42 +0300

    Add database configurations

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 """
 A module containing the application's Config class
 """
+import os
+
+
 class Config(object):
     """
-    The config class contains the application's various configurations 
+    The config class contains the application's various configurations
     """
-    pass
+    DB_USERNAME = os.environ.get('DB_USERNAME')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
