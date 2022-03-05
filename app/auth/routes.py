e014707b02c070df25fb87f56f6036778f963567
OyugoObonyo <nyikal1999@gmail.com>
2022-03-05 13:35:38 +0300

    create auth routes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,9 +1,11 @@
 """
 A module with views related to authorization and authentication
 """
-from crypt import methods
 from flask import current_app, render_template, redirect
-from app.auth import bp
+from auth import bp
+from auth.forms import RegistrationForm
+from app.models.user import User
+import
 
 
 @bp.route('/register', methods=['GET', 'POST'])
@@ -11,7 +13,13 @@ def register():
     """
     route that handles the user registration action
     """
-    pass
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data
+        )
+    return render_template('auth/register.html', title="Register", form=form)
 
 
 @bp.route('/login')
@@ -19,4 +27,4 @@ def login():
     """
     route that handles user login action
     """
-    pass
+    return render_template('auth/login.html', title="Log In")
