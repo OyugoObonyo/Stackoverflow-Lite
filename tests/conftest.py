df32323d850c01dcbac5540075979f0747a45a33
OyugoObonyo <nyikal1999@gmail.com>
2022-03-07 12:46:23 +0300

    Add secret key to app instance

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 """
 Fixtures for the pytest module
 """
-import pytest
 from app import create_app
+import os
+import pytest
 
 
 @pytest.fixture(scope="module")
@@ -11,5 +12,11 @@ def test_client():
     creates an instance of flask application to be used
     """
     app = create_app()
+    SECRET_KEY = os.urandom(32)
+    app.config['SECRET_KEY'] = SECRET_KEY
+    # Establish an application context
+    ctx = app.app_context()
+    ctx.push()
     with app.test_client() as c:
         yield c
+    ctx.pop()
