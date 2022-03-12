eaab02f638f777cedafab88e424d98256b91a247
OyugoObonyo <nyikal1999@gmail.com>
2022-03-12 11:12:52 +0300

    Distinguish application configurations

--- config.py
+++ config.py
@@ -1,13 +1,52 @@
 """
 A module containing the application's Config class
 """
+
 import os
 
 
 class Config(object):
     """
-    The config class contains the application's various configurations
+    The config class contains the application's default configurations
     """
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    DB_HOST = 'localhost'
     DB_USERNAME = os.environ.get('DB_USERNAME')
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DEBUG = True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class DevelopmentConfig(Config):
+    """
+    Application's configurations during development
+    """
+
+    DB_NAME = os.environ.get('DB_DEV_NAME')
+    DB_PASSWORD = os.environ.get('DB_DEV_PASSWORD')
+    DB_USERNAME = os.environ.get('DB_DEV_USERNAME')
+
+
+class TestingConfig(Config):
+    """
+    Application's configurations for testing
+    """
+
+    DB_NAME = os.environ.get('DB_TEST_NAME')
+    DB_PASSWORD = os.environ.get('DB_TEST_PASSWORD')
+    DB_USERNAME = os.environ.get('DB_TEST_USERNAME')
+
+
+class ProductionConfig(Config):
+    """
+    Application's configurations during production
+    """
+    pass
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
