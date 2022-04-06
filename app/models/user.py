3734396a8f9aa838a6c6f6ffe46cd3a4d075d1a0
OyugoObonyo <nyikal1999@gmail.com>
2022-04-06 16:08:30 +0300

    Enforce authentication by use of tokens

--- app/models/user.py
+++ app/models/user.py
@@ -15,7 +15,9 @@ class User:
     The user's data model
     """
 
-    def __init__(self, username, email, password_hash=None, created_at=None, id=None):
+    def __init__(
+        self, username, email, password_hash=None, created_at=None, id=None
+    ):
         """
 
         Initialzing the user class
@@ -28,7 +30,7 @@ class User:
         self.id = id
 
     def get_token(self, user_id):
-        """ "
+        """"
 
         get_token generates a token for a user to be used for authentication
         :user_id: id of user whose token is to be generated
@@ -37,17 +39,19 @@ class User:
 
         try:
             payload = {
-                "exp": datetime.datetime.utcnow()
-                + datetime.timedelta(days=0, seconds=5),
-                "iat": datetime.datetime.utcnow(),
-                "sub": user_id,
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
             }
             return jwt.encode(
-                payload, current_app.config["SECRET_KEY"], algorithm="HS256"
+                payload,
+                current_app.config['SECRET_KEY'],
+                algorithm='HS256'
             )
         except Exception as e:
             return e
 
+    
     @staticmethod
     def decode_auth_token(auth_token):
         """
@@ -56,9 +60,9 @@ class User:
         :return: id of user associated with the token or an error string
         """
         try:
-            payload = jwt.decode(auth_token, current_app.config("SECRET_KEY"))
-            return payload["sub"]
+            payload = jwt.decode(auth_token, current_app.config('SECRET_KEY'))
+            return payload['sub']
         except jwt.ExpiredSignatureError:
-            return "Signature expired. Please log in again."
+            return 'Signature expired. Please log in again.'
         except jwt.InvalidTokenError:
-            return "Invalid token. Please log in again."
+            return 'Invalid token. Please log in again.'
