3c0a0f019c16694bc625f5cdc7745133840defcc
OyugoObonyo <nyikal1999@gmail.com>
2022-04-07 16:44:40 +0300

    Create registration view

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,21 +6,36 @@ routes that handle user authentication
 from app.auth import bp
 from app.models.user import User
 from app.models.sql_user_query_model import SqlUserQueryModel
-from flask import request
+from flask import make_response, request
+from utils.passwords import hashing
 
 
-@bp.route('/register', methods=['POST'])
+@bp.route("/register", methods=["POST"])
 def register():
     """
-    
+
     register route handles user registration to the database
     """
 
     response = request.get_json()
-    user = SqlUserQueryModel.get_by_name(response['username'])
+    user = SqlUserQueryModel.get_by_name(response["username"])
     if user is None:
         user = User(
-            username = response['username'],
-            email = response['email'],
-            password_hash = response['password'])
+            username=response["username"],
+            email=response["email"],
+            password_hash=hashing.generate_password_hash(response["password"]),
+        )
         SqlUserQueryModel.save(user)
+        response = {
+            "status": "success",
+            "account_information": {
+                "name": user.username,
+                "email": user.email
+            }
+        }
+    else:
+        response = {
+            "status": "fail",
+            "message": "A user with these credentials already exists"
+        }
+        return make_response(response), 401
