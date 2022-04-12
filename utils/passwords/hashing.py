d85db192e8b7099bd1013718b994d5e28cbea15a
OyugoObonyo <nyikal1999@gmail.com>
2022-04-12 13:23:00 +0300

    enforce check password hash functionality

--- utils/passwords/hashing.py
+++ utils/passwords/hashing.py
@@ -9,13 +9,27 @@ import bcrypt
 
 def generate_password_hash(password: str):
     """
-    
+
     generate_password_hash generates a hash from a given password
     :password: password to be hashed
     :return: a hash of the given password of type bytes
     """
 
-    password_byte = password.encode('utf-8')
+    password_byte = password.encode("utf-8")
     salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password_byte, salt)
-    return hashed
+    hash = bcrypt.hashpw(password_byte, salt)
+    return hash.decode("utf-8")
+
+
+def check_password_hash(password: str, hash: bytes) -> bool:
+    """
+
+    check_password_hash checks whether a password matches a particular hash
+    :password: password to be checked
+    :hash: hash of given password
+    :return: True if password matches and false if it doesn't
+    """
+
+    encoded_password = password.encode("utf-8")
+    match = bcrypt.checkpw(encoded_password, hash.encode("utf-8"))
+    return match
