3bacff8b0d748f95a5364fa28e0fd6a3141589ec
OyugoObonyo <nyikal1999@gmail.com>
2022-04-13 12:13:46 +0300

    create query models

---
+++ app/models/sql_query_models/sql_blacklisted_token_query_model.py
@@ -0,0 +1,21 @@
+"""
+
+The sql_blacklisted_token_query_model module: Contains
+SqlBlacklistedTokenQueryModel class
+
+This module demonstrates implementation of a class that is
+useful in querying the database's blacklisted_tokens table
+"""
+
+from abc import ABC, abstractmethod
+from utils.db import modify_db
+
+
+class BlacklistedTokenQueryModel(ABC):
+    """
+    
+
+    """
+    pass
+
+
